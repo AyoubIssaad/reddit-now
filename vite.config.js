4ba0ed2c2824cd6554412d45b2877735168dc134
ayoubissaad <ayoub.issaad@gmail.com>
2024-12-25 16:09:24 +0100

    routeFix3 _redirects

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { copyFileSync } from "fs";
+
+// Copy _redirects to dist during build
+const copyRedirects = () => {
+  return {
+    name: "copy-redirects",
+    closeBundle() {
+      copyFileSync("public/_redirects", "dist/_redirects");
+    },
+  };
+};
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), copyRedirects()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
